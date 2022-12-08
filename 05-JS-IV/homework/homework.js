b5dc717290e3cb687e7ddcf94aea5e4205d2269c
turbulenci <jesusdanielyguaran@gmail.com>
2022-12-08 12:32:56 -0300

    modulo 4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -30,7 +30,7 @@ function invocarMetodo (objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
-  // Tu código:
+  // Tu 
   objeto[metodo]()
 }
 
